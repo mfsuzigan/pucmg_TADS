30d8f301ef5b6c207822f150ea027982a804fa13
Michel Suzigan <mfsuzigan@gmail.com>
2023-02-20 22:10:30 -0300

    Criacao do repo

---
+++ eixo_1/microfundamentos/algoritmos_abstracao_dados/EstruturasHomogeneas/Program.cs
@@ -0,0 +1,12 @@
+﻿partial class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 0;
+        while (i++ <10)
+        {
+            Console.WriteLine("It's working ( I guess)");
+        }
+        Console.ReadLine();
+    }
+}
